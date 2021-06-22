d6b9933199b3f9dad9fc0cc5050d27e2840921ff
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-22 14:45:45 -0300

    Q8

--- unique/include/unique.h
+++ unique/include/unique.h
@@ -16,21 +16,17 @@ namespace graal {
 template<class InputIt, class Equal>
 InputIt unique( InputIt first, InputIt last, Equal eq )
 {
-    int *i,*j,*last_temp;
-    int var=0;
-    for(i=first; i<last; ++i){
-        var=0;
-        for(j=first; j<i; ++j){
+    auto last_temp=last;
+
+    for(auto i=first; i<last; ++i)
+        for(auto j=first; j<i; ++j)
             if(eq(*i,*j)){
-                var=1;
+                last_temp=i;
+                copy(i+1,last,last_temp);
+                last=last_temp;
                 break;
             }
-        }if(var==1){
-            last_temp=i;
-            copy(i+1,last,last_temp);
-            last=last_temp;
-        }
-    }
+
     return last;
 }
 
