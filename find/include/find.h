daf230409d848f85545a30a13cff52297d551ab1
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-21 23:32:41 -0300

    Q5

---
+++ find/include/find.h
@@ -0,0 +1,30 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class T, class Equal>
+InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
+{
+    for(int *i=first; i<=last; ++i){
+        if(i==last)
+            return last;
+        if(eq(*i,value))    
+            return i;
+    }
+    // TODO
+    return first;
+}
+
+}
+#endif
