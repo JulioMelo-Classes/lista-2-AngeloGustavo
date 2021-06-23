6d06b388fc43db94b66f5393da701df6e3101028
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-23 02:35:45 -0300

    Q6

---
+++ all_any_none_of/include/all_any_none_of.h
@@ -0,0 +1,51 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+bool all_of(InputIt first, InputIt last, UnaryPredicate p)
+{
+    for(auto i=first; i<=last; ++i)
+        if(!p(*i))
+            return false;
+    return true;
+
+}
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+bool any_of(InputIt first, InputIt last, UnaryPredicate p)
+{
+    for(auto i=first; i<=last; ++i)
+        if(p(*i))
+            return true;
+    return false;
+}
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+bool none_of(InputIt first, InputIt last, UnaryPredicate p)
+{
+    for(auto i=first; i<=last; ++i)   
+        if(p(*i))
+            return false;
+    return true;
+}
+
+}
+#endif
