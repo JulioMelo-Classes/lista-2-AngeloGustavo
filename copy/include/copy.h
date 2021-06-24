f261f05bf348cd9debf63734185d62894af46af3
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:37:20 -0300

    Adição do Doxygen

--- copy/include/copy.h
+++ copy/include/copy.h
@@ -11,7 +11,13 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param first Ponteiro para se tornar o primeiro elemento do range após a reordenação
+ * @return Ponteiro para a posição logo após o último elemento do range reordenado
+ *
  */
 template<class InputIt>
 InputIt copy(InputIt first, InputIt last, InputIt d_first)
