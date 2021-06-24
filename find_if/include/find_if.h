99fbeb03a258d892b32a834a7fd0a12d8727d4f1
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:38:13 -0300

    Adição do Doxygen

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -11,7 +11,17 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * Exemplo de documentação seguindo o formato Doxygen
+ *
+ * @tparam InputIt iterator para o range.
+ * @tparam UnaryPredicate o tipo de uma função bool(const Type &a)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param p A função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário
+ *
+ * @return Um iterator apontando para o primeiro elemento do range que satisfaz p
+ *
  */
 template<class InputIt, class UnaryPredicate>
 InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
