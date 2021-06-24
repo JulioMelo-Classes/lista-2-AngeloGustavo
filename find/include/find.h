5400074e2608bd53f7e111bc9ec1991fcbad01a9
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:37:56 -0300

    Adição do Doxygen

--- find/include/find.h
+++ find/include/find.h
@@ -11,7 +11,16 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam Equal o tipo de uma função bool eq( const Type &a, const Type &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param value Inteiro valor para ser procurado no range 
+ * @param eq A função que retorna true se o elemento é igual ao segundo, ou falso caso contrário
+ *
+ * @return Um iterator para o primeiro elemento que satisfaz a função eq
+ *
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
