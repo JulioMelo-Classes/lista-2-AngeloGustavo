449e9ac7ef8922f265b1bd4153b70bc3d0be9ad6
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:38:28 -0300

    Adição do Doxygen

--- partition/include/partition.h
+++ partition/include/partition.h
@@ -11,7 +11,15 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam ForwardIt iterator para o range.
+ * @tparam UnaryPredicate o tipo de uma função bool(const string &a)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param p A função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário
+ *
+ * @return Um iterator apontando para o elemento logo após o último elemento no range reordenado que satisfaz o predicado
+ *
  */
 template<class ForwardIt, class UnaryPredicate>
 ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
