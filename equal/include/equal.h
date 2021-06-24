ed4c8329c036db5963c0c3341811b86a388055d7
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:37:41 -0300

    Adição do Doxygen

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -11,7 +11,17 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt1 iterator para o range.
+ * @tparam InputIt1 iterator para o range.
+ * @tparam Equal o tipo de uma função bool(const string &a,  const string &b)
+ *
+ * @param first1 Ponteiro para o primeiro elemento do range
+ * @param last1 Ponteiro para a posição logo após o último elemento do range
+ * @param first2 Ponteiro para o primeiro elemento do range que deve ser levado em consideração para a comparação
+ * @param eq A função que retorna true se o elemento é igual ao segundo, ou falso caso contrário
+ *
+ * @return True se os ranges são iguais, false caso contrário
+ *
  */
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
