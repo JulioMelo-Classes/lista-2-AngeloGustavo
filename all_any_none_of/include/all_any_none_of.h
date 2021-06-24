c3de05a462c530ee71a4dc20c56bcd60e182d7a6
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:37:02 -0300

    Adição do Doxygen

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -11,7 +11,15 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam UnaryPredicate o tipo de uma função bool(const string &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param p A função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário
+ *
+ * @return True caso todos os elementos do range obedecerem a determinada condição ou falso caso contrário
+ *
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
@@ -24,7 +32,15 @@ bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 }
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam UnaryPredicate o tipo de uma função bool(const string &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param p A função que retorna true quando uma determinada condição é satisfeita ou false caso contrário
+ *
+ * @return True caso algum dos elementos do range obedecerem a determinada condição ou false caso contrário
+ *
  */
 template<class InputIt, class UnaryPredicate>
 bool any_of(InputIt first, InputIt last, UnaryPredicate p)
@@ -36,7 +52,15 @@ bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 }
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam UnaryPredicate o tipo de uma função bool(const string &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param p A função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário
+ *
+ * @return True caso todos os elementos do range desobedecerem a determinada condição ou falso caso contrário
+ *
  */
 template<class InputIt, class UnaryPredicate>
 bool none_of(InputIt first, InputIt last, UnaryPredicate p)
