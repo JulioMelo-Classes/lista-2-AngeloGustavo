a26da2aa90be20f691d18c1f58501da8d83b357c
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-24 03:39:16 -0300

    Adição do Doxygen

--- unique/include/unique.h
+++ unique/include/unique.h
@@ -11,7 +11,15 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam Equal o tipo de uma função bool(const string &a,  const string &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param eq A função que retorna true se o elemento é igual ao segundo, ou falso caso contrário
+ *
+ * @return Um iterator para o elemento logo após o último elemento do range após o processamento.
+ *
  */
 template<class InputIt, class Equal>
 InputIt unique( InputIt first, InputIt last, Equal eq )
